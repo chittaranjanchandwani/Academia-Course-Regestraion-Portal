b9bf0303b602b984204ef098fe3f42908c104b02
chittaranjanchandwani <89841114+chittaranjanchandwani@users.noreply.github.com>
2023-10-18 15:45:51 +0530

    added structures

---
+++ structures/enrollment.h
@@ -0,0 +1,13 @@
+#ifndef ENROLLMENT_RECORD
+#define ENROLLMENT_RECORD
+
+struct enrollment
+{
+    int stid; // 0, 1, 2 ....
+    int cid;
+    //char name[20];
+    char status[12];
+    //time_t enroll_time;    
+};
+
+#endif
